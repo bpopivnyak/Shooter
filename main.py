def0ee2524b6cdf427990faa6c1b561340b99e55
YuriyKulyck <141341450+bpopivnyak@users.noreply.github.com>
2023-12-17 18:28:15 +0200

    samolet

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+window = pygame.display.set_mode((800, 500))
+fps = pygame.time.Clock()
+
+
+game = True
+while game:
+
+
+
+
+
+    window.fill((123,123,123))
+
+    pygame.displat.flip()
+    fps.tick(60)
