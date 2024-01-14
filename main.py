b341a4963525e7a59708910cfde825a524bc8410
lpopivnyak <141341450+bpopivnyak@users.noreply.github.com>
2024-01-14 18:28:51 +0200

    GG

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-
 import rocket
+import ufo
+import asteroid
 
 pygame.init()
 window = pygame.display.set_mode((1370, 705))
@@ -16,8 +17,19 @@ backgroundImage = pygame.transform.scale(backgroundImage, (1370, 705))
 
 Rocket = rocket.Character(600, 598, 70, 100, 10, "rocket.png")
 Ufo = []
+Asteroid = []
 Ufo.append(ufo.Enemy(50, 50, 70, 80, 10, "ufo.png"))
-
+Asteroid.append(asteroid.Enemy(150, 50, 70, 80, 10, "asteroid.png"))
+Ufo.append(ufo.Enemy(250, 50, 70, 80, 10, "ufo.png"))
+Asteroid.append(asteroid.Enemy(370, 50, 70, 80, 10, "asteroid.png"))
+Ufo.append(ufo.Enemy(470, 50, 70, 80, 10, "ufo.png"))
+Asteroid.append(asteroid.Enemy(590, 50, 70, 80, 10, "asteroid.png"))
+Ufo.append(ufo.Enemy(690, 50, 70, 80, 10, "ufo.png"))
+Asteroid.append(asteroid.Enemy(810, 50, 70, 80, 10, "asteroid.png"))
+Ufo.append(ufo.Enemy(910, 50, 70, 80, 10, "ufo.png"))
+Asteroid.append(asteroid.Enemy(1030, 50, 70, 80, 10, "asteroid.png"))
+Ufo.append(ufo.Enemy(1130, 50, 70, 80, 10, "ufo.png"))
+Asteroid.append(asteroid.Enemy(1250, 50, 70, 80, 10, "asteroid.png"))
 
 game = True
 while game:
@@ -28,12 +40,17 @@ while game:
 
 
     Rocket.move()
-    ufo.move()
+    for ufo in Ufo:
+        ufo.move()
+    for asteroid in Asteroid:
+        asteroid.move()
 
     window.fill((123,123,123))
     window.blit(backgroundImage, (0, 0))
     Rocket.render((window))
     for ufo in Ufo:
-        ufo.Render(window)
+        ufo.render(window)
+    for asteroid in Asteroid:
+        asteroid.render(window)
     pygame.display.flip()
     fps.tick(60)
